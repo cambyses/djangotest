4ba45d6dfa038c0fd77ce7cae17c090f9045d0cb
cambyses <kambizbehi@gmail.com>
2017-04-22 08:11:39 -0400

    Demo

--- Gallery/Page/views.py
+++ Gallery/Page/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import Person
+import requests
+
 
 # Create your views here.
 def index(request):
     people = Person.objects.all()
     return render(request, 'index.html', {'people':people})
+
+#def index(request):
+#    r = requests.get('http://httpbin.org/status/418')
+#    print(r.text)
+#    return HttpResponse('<pre>' + r.text + '</pre>')
